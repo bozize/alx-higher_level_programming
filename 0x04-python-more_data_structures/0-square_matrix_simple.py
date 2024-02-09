d57944884e0e56cd2632c1c2c2002cc5e149ec50
bozize <abdiazizibrahim56@.com>
2024-02-09 11:46:09 -0800

    whoo

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for lst in matrix:
+        new_matrix.append(list(map(lambda x: x**2, lst)))
+
+    return new_matrix
